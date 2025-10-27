cb2ab953ee7b8959f0e621e5123621bd4213e288
aigle0 <omar.elbettah@gmail.com>
2025-10-27 15:55:28 +0100

    Upload FeedBack and Create OpenApi for my app

---
+++ BsnBetaBackEnd/src/main/java/com/bsn/beta/feedback/FeedbackMapper.java
@@ -0,0 +1,29 @@
+package com.bsn.beta.feedback;
+
+import com.bsn.beta.saas.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+
+    public Feedback toFeedback( FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .build()
+                )
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer userId) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), userId))
+                .build();
+    }
+}
---
+++ BsnBetaBackEnd/src/main/java/com/bsn/beta/feedback/FeedbackService.java
@@ -0,0 +1,62 @@
+package com.bsn.beta.feedback;
+
+import com.bsn.beta.exception.OperationNotPermittedException;
+import com.bsn.beta.saas.Book;
+import com.bsn.beta.saas.BookRepository;
+import com.bsn.beta.saas.PageResponse;
+import com.bsn.beta.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+
+    private final BookRepository bookRepository;
+    private final FeedbackMapper feedbackMapper;
+    private final FeedbackRepository feedbackRepository;
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(()-> new EntityNotFoundException("No book found with ID:: " + request.bookId() ));
+        if(book.isArchived() || !book.isShareable()){
+            throw new OperationNotPermittedException("You cannot give a feedback for an archived or not shareable book");
+        }
+        User user = (User) connectedUser.getPrincipal();
+        if(Objects.equals(user.getId(), book.getOwner().getId())){
+            throw new OperationNotPermittedException("You cannot give a feedback for your own book");
+        }
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedbackRepository.save(feedback).getId();
+    }
+
+    public PageResponse<FeedbackResponse> findAllFeedbackByBook(Integer bookId,
+                                                                int page,
+                                                                int size,
+                                                                Authentication connectedUser) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        User user = (User) connectedUser.getPrincipal();
+        Page<Feedback> feedbacks = feedbackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(f -> feedbackMapper.toFeedbackResponse(f,user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
+    }
+}
